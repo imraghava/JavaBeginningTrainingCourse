a392bca352485079eb91c2eb66a379dee778dccd
alex <alexander.tyshchenko@gmail.com>
2014-12-20 22:23:06 +0200

    added lesson 5

---
+++ src/com/tyshchenko/java/beginning/lesson5/VisualVmExample.java
@@ -0,0 +1,15 @@
+package com.tyshchenko.java.beginning.lesson5;
+
+/**
+ * author Alexander Tyshchenko.
+ */
+public class VisualVmExample {
+    public static void main(String[] args) {
+        MyClass clazz = new MyClass();
+        while(true){}
+    }
+
+    public static class MyClass {
+        String s = "mystring";
+    }
+}
