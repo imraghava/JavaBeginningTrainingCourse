639d431d0ffa0215b7eaa80eef542934e6cd0973
alex <alexander.tyshchenko@gmail.com>
2015-02-23 22:05:39 +0200

    small fixes

--- src/main/java/com/tyshchenko/java/training/oop/lesson4/interfaces/TimeClientInterfaceStaticMethod.java
+++ src/main/java/com/tyshchenko/java/training/oop/lesson4/interfaces/TimeClientInterfaceStaticMethod.java
@@ -20,7 +20,7 @@ public interface TimeClientInterfaceStaticMethod {
         try {
             return ZoneId.of(zoneString);
         } catch (DateTimeException e) {
-            System.err.println("Invalid time zone: " + zoneString +
+            System.out.println("Invalid time zone: " + zoneString +
                     "; using default time zone instead.");
             return ZoneId.systemDefault();
         }
