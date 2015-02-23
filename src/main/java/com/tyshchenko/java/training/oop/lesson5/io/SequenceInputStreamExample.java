617029593c20079f41ef0d7819aae84a4e276463
alex <alexander.tyshchenko@gmail.com>
2015-02-23 15:58:39 +0200

    added SequenceInputStreamExample

---
+++ src/main/java/com/tyshchenko/java/training/oop/lesson5/io/SequenceInputStreamExample.java
@@ -0,0 +1,63 @@
+package com.tyshchenko.java.training.oop.lesson5.io;
+
+import com.tyshchenko.java.training.oop.Constants;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ * @author Alexander Tyshchenko.
+ */
+public class SequenceInputStreamExample {
+
+    public static void main(String[] args) {
+        int c;
+        Vector<String> files = new Vector<>();
+
+        files.addElement(Constants.FILE_PATH + "/file1.txt");
+        files.addElement(Constants.FILE_PATH + "/file2.txt");
+        files.addElement(Constants.FILE_PATH + "/file3.txt");
+        InputStreamEnumerator ise = new InputStreamEnumerator(files);
+        InputStream input = new SequenceInputStream(ise);
+
+        try {
+            while ((c = input.read()) != -1)
+                System.out.print((char) c);
+        } catch(NullPointerException e) {
+            System.out.println("Error Opening File.");
+        } catch(IOException e) {
+            System.out.println("I/O Error: " + e);
+        } finally {
+            try {
+                input.close();
+            } catch(IOException e) {
+                System.out.println("Error Closing SequenceInputStream");
+            }
+        }
+    }
+
+
+    private static class InputStreamEnumerator implements Enumeration<FileInputStream> {
+        private Enumeration<String> files;
+
+        public InputStreamEnumerator(Vector<String> files) {
+            this.files = files.elements();
+        }
+
+        public boolean hasMoreElements() {
+            return files.hasMoreElements();
+        }
+
+        public FileInputStream nextElement() {
+            try {
+                return new FileInputStream(files.nextElement().toString());
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+}
