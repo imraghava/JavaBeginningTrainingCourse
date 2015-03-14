3aa80fd3b62b72a65ddfc95c221b49e4c49dde38
alex <alexander.tyshchenko@gmail.com>
2015-03-14 23:51:35 +0200

    small changes

--- src/main/java/com/tyshchenko/java/training/oop/lesson11/rmi/client/ClientCalculator.java
+++ src/main/java/com/tyshchenko/java/training/oop/lesson11/rmi/client/ClientCalculator.java
@@ -3,6 +3,8 @@ package com.tyshchenko.java.training.oop.lesson11.rmi.client;
 import com.tyshchenko.java.training.oop.lesson11.rmi.common.Calculator;
 import com.tyshchenko.java.training.oop.lesson11.rmi.common.RMIConstants;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -13,10 +15,13 @@ import java.rmi.registry.Registry;
  */
 public class ClientCalculator {
 
-    public static void main(String[] args) throws RemoteException, NotBoundException {
-        Registry registry = LocateRegistry.getRegistry("localhost", 1199);
+    public static void main(String[] args) throws RemoteException, NotBoundException, UnknownHostException {
+        Registry registry = LocateRegistry.getRegistry(RMIConstants.RMI_PORT);
         Calculator calculator = (Calculator) registry.lookup(RMIConstants.RMI_SERVICE);
         System.out.println(calculator.add(1, 2));
+
+//        check ip in the same network
+//        System.out.println(InetAddress.getLocalHost().getHostAddress());
     }
 
 }
