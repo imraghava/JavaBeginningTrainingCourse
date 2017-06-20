534d6e36dae4ea2cc49fed306adc7a2cc77557c0
alexander.tyshchenko <alexander.tyshchenko@gmail.com>
2017-06-20 16:14:34 +0300

    added HowManyExercises counter

---
+++ src/main/java/com/tyshchenko/training/java/beginning/HowManyExercises.java
@@ -0,0 +1,32 @@
+package com.tyshchenko.training.java.beginning;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Alexander Tyshchenko
+ */
+
+public class HowManyExercises {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("./Exercises.md"))) {
+            int exercises = 0;
+            for (String line; (line = reader.readLine()) != null;) {
+                if (line.length() == 0)
+                    continue;
+                if (line.charAt(0) == '#') {
+                    if (exercises != 0) {
+                        System.out.println("Exercises - " + exercises);
+                        exercises = 0;
+                    }
+                    System.out.println("Section - " + line.substring(3));
+                    continue;
+                }
+                if (line.charAt(0) == '-')
+                    ++exercises;
+            }
+            System.out.println("Exercises - " + exercises);
+        }
+    }
+}
